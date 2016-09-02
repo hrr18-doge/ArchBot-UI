6b8cc8f83648ea8a982108d658183a0f874e9c98
Michael Daof <michael.ivanhoe@gmail.com>
2016-09-02 16:03:44 -0700

    (feat) setup view fiels

---
+++ public/views/components/App.jsx
@@ -0,0 +1,17 @@
+class App extends React.Component {
+  
+  // classes are usd for stateful components
+  constructor(props){
+    super(props)
+    // set state
+  }
+
+  render (){
+    // primary layout
+    <div>
+      <Account/>
+      <Search/>
+      <Results/>
+    </div>
+  }
+}
