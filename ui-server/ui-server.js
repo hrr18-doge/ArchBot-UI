05eb828dcd8053392e3022230c486ff742404893
Michael Daof <michael.ivanhoe@gmail.com>
2016-09-06 14:28:18 -0700

    preparing rebase

--- ui-server/ui-server.js
+++ ui-server/ui-server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var db = require('../db/config.js');
 
 var app = express();
 var port = process.env.PORT || 4568;
